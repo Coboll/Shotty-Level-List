298f4cf6fb691d5cf05c73eb18320d2d68ba6344
Cody <65142939+Coboll@users.noreply.github.com>
2024-02-21 20:59:55 -0800

    Update levels-main.js

--- level/levels-main.js
+++ level/levels-main.js
@@ -11,7 +11,7 @@ const d = {
               },
 
           ],
-          "name": "Shotty somthing",
+          "name": "Shotty Cobwebs",
           "author": "Coboll",
           "more": "None",
           "id": 83870024,
